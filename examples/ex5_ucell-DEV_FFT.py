821cc8c8f9a8d2c240224b798889bbdc1e3eac1d
yonghakim <yonghakim.t@gmail.com>
2022-10-19 21:35:00 +0900

    adding piecewise constant FT

--- examples/ex5_ucell-DEV_FFT.py
+++ examples/ex5_ucell-DEV_FFT.py
@@ -6,7 +6,7 @@ import numpy as np
 from meent.rcwa import call_solver, sweep_wavelength
 
 
-grating_type = 0  # 0: 1D, 1: 1D conical, 2:2D.
+grating_type = 2  # 0: 1D, 1: 1D conical, 2:2D.
 pol = 1  # 0: TE, 1: TM
 
 n_I = 1  # n_incidence
@@ -20,7 +20,7 @@ wls = np.linspace(900, 900, 1)  # wavelength
 
 if grating_type in (0, 1):
     period = [1400]
-    fourier_order = 20
+    fourier_order = 3
 
 else:
     period = [700, 700]
@@ -31,18 +31,18 @@ thickness = [460, 660]
 ucell = np.array([
 
     [
+        [1, 1, 1, 0, 0, 0, 0, 1, 1, 0],
+        [1, 1, 1, 0, 0, 0, 1, 1, 1, 0],
         [1, 1, 1, 0, 0, 0, 1, 1, 1, 1],
-        # [1, 1, 1, 0, 0, 0, 1, 1, 1, 1],
-        # [1, 1, 1, 0, 0, 0, 1, 1, 1, 1],
     ],
-    [
-        [1, 1, 1, 0, 0, 0, 1, 1, 1, 1],
-        # [1, 1, 1, 0, 0, 0, 1, 1, 1, 1],
-        # [1, 1, 1, 0, 0, 0, 1, 1, 1, 1],
-    ],
+    # [
+    #     [1, 1, 1, 0, 0, 0, 1, 1, 1, 1],
+    #     [1, 1, 1, 0, 0, 0, 1, 1, 1, 1],
+    #     [1, 1, 1, 0, 0, 0, 1, 1, 1, 1],
+    # ],
 ])
 
-ucell_materials = ['Si__real', 1]
+ucell_materials = [1, 1.5]
 
 AA = call_solver(mode=0, grating_type=grating_type, pol=pol, n_I=n_I, n_II=n_II, theta=theta, phi=phi, psi=psi,
                  fourier_order=fourier_order, wls=wls, period=period, ucell=ucell, ucell_materials=ucell_materials,
@@ -51,12 +51,9 @@ de_ri, de_ti = AA.run_ucell()
 print(de_ri, de_ti)
 
 wls = np.linspace(500, 1000, 100)
-t0 = time.perf_counter()
 
 a, b = sweep_wavelength(wls, mode=0, grating_type=grating_type, pol=pol, n_I=n_I, n_II=n_II, theta=theta, phi=phi, psi=psi,
                  fourier_order=fourier_order, period=period, ucell=ucell, ucell_materials=ucell_materials, thickness=thickness)
-print('wall time: ', time.perf_counter() - t0)
 
-# plt.plot(wls, a.sum((1, 2)), wls, b.sum((1, 2)))
 plt.plot(wls, a.sum((1)), wls, b.sum((1)))
 plt.show()
